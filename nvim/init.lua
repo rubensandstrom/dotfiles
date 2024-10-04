9b19869cbca38007acb2b1dd82dacea420dc7aa3
Ruben Sandström <rubensandstrom@gmail.com>
2024-10-04 18:38:15 +0200

    backup

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,6 +20,16 @@ vim.api.nvim_create_autocmd("BufRead", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("BufRead", {
+	group = vim.api.nvim_create_augroup("detect_gitconfig", { clear = true }),
+	-- desc = 'Set filetype for gitconfig-* files',
+	pattern = { "*.v" },
+	callback = function()
+		vim.cmd("set filetype=verilog")
+	end,
+})
+
 require("options")
 require("keymaps")
 require("lazy").setup("plugins")
+
